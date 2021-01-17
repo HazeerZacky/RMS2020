e8a90f05f76976e880ba3c91f000efe5a2822df1
Hazeer Zacky <65429076+hazeerzacky@users.noreply.github.com>
2021-01-17 11:43:35 +0530

    HAZKY - Form

--- resources/views/Pages/Student.blade.php
+++ resources/views/Pages/Student.blade.php
@@ -10,8 +10,6 @@
   <link rel="stylesheet" href="{{asset('template')}}/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<!-- daterange picker -->
-<link rel="stylesheet" href="{{asset('template')}}plugins/daterangepicker/daterangepicker.css">
   <!-- DataTables -->
   <link rel="stylesheet" href="{{asset('template')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="{{asset('template')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
@@ -337,24 +335,23 @@
                             <input type="text" class="form-control"name="SName" placeholder="Enter student name">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputText" class="form-label">Student Status</label><br>
-                            <div class="custom-control custom-radio custom-control-inline">
-                              <input type="radio" id="customRadioInline1" value="Active" name="SStatus" class="custom-control-input">
-                              <label class="custom-control-label" for="customRadioInline1">Active</label>
-                            </div>
-                            <div class="custom-control custom-radio custom-control-inline">
-                              <input type="radio" id="customRadioInline2" value="Deactive" name="SStatus" class="custom-control-input">
-                              <label class="custom-control-label" for="customRadioInline2">Deactive</label>
-                            </div>
+                            <label for="exampleInputText" class="form-label">Gender</label>
+                            <input type="text" class="form-control"name="SName" placeholder="Enter gender">
                         </div>
                         <div class="form-group">
-                        <label>Date:</label>
-                            <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                                <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
-                                <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                </div>
-                            </div>
+                            <label for="exampleInputText" class="form-label">Date of birth</label>
+                            <input type="text" class="form-control"name="SName" placeholder="Enter dob">
+                        </div>
+                        
+                        <div class="form-group">
+                        <label>Class ID</label>
+                            <select class="custom-select" name="CType">
+                                <option value="" selected disabled hidden>(select one option)</option>
+                                <option value="GCE-A/L"><b>GCE Advanced Level</b></option>
+                                <option value="GCE-O/L"><b>GCE Ordinary Level</b></option>
+                                <option value="SecondaryLevel"><b>Secondary Level</b></option>
+                                <option value="PrimaryLevel"><b>Primary Level</b></option>
+                            </select>
                         </div>
             </div>
                                 <div class="modal-footer">
@@ -588,8 +585,6 @@
 <script src="{{asset('template')}}/dist/js/demo.js"></script>
 <!-- Toastr -->
 <script src="{{asset('template')}}/plugins/toastr/toastr.min.js"></script>
-<!-- daterange-picker -->
-<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
 <!-- overlayScrollbars -->
 <script src="{{asset('template')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- ====================================      Include Scrips Part End      ========================================= -->
@@ -647,82 +642,10 @@
           </script>
           @endforeach
         @endif
+
+
+
 <!-- Alert Part End -->
-<!-- datepicker start-->
-<script>
-  $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-      theme: 'bootstrap4'
-    })
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date range picker
-    $('#reservationdate').datetimepicker({
-        format: 'L'
-    });
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({
-      timePicker: true,
-      timePickerIncrement: 30,
-      locale: {
-        format: 'MM/DD/YYYY hh:mm A'
-      }
-    })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Timepicker
-    $('#timepicker').datetimepicker({
-      format: 'LT'
-    })
-    
-    //Bootstrap Duallistbox
-    $('.duallistbox').bootstrapDualListbox()
-
-    //Colorpicker
-    $('.my-colorpicker1').colorpicker()
-    //color picker with addon
-    $('.my-colorpicker2').colorpicker()
-
-    $('.my-colorpicker2').on('colorpickerChange', function(event) {
-      $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
-    });
-
-    $("input[data-bootstrap-switch]").each(function(){
-      $(this).bootstrapSwitch('state', $(this).prop('checked'));
-    });
-
-  })
-</script>
-<!-- datepicker end-->
 <!-- page script Part End-->
 </body>
 </html>
